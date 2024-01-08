c2b51e27ab50b8b0471bdeb811de6632942de294
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-08 15:28:08 +0100

    add gitInfo function

--- classes.php
+++ classes.php
@@ -3,15 +3,14 @@ declare(strict_types=1);
 
 //EXERCISE 1
 //TODO: Create a class beverage.
-//TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
-//TODO: Have a default value "cold" in the construct for temperature.
-
 class Beverage
 {
     public $color;
     public $price;
     public $temperature;
 
+//TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
+//TODO: Have a default value "cold" in the construct for temperature.
 public function __construct(string $color, float $price, string $temperature = 'cold')
 {
     $this->color = $color;
@@ -19,14 +18,17 @@ public function __construct(string $color, float $price, string $temperature = '
     $this->temperature = $temperature;
 }
 
-}
-
-
-
-
-//Remember for now we will use properties and methods that can be accessed from everywhere.
 //TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
+    public function getInfo(){
+    {
+        return "This beverage is {$this->temperature} and {$this->color}.";
+    }
+    }
+}
 //TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
+$cola = new Beverage('black', 2.0);
+
+
  //print the getInfo on the screen.
 //TODO: Print the temperature on the screen.
 
