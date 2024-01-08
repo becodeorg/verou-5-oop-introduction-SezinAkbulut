65dfbc9d6a45d5198c4a13fef4b7d0314cac3209
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-08 18:37:38 +0100

    update properties to protected

--- private.php
+++ private.php
@@ -59,7 +59,7 @@ class Beer extends Beverage
 
     private function beerInfo(): string
     {
-        return "Hi, I'm {$this->name} and have an alcohol percentage of {$this->alcoholPercentage} and I have a {$this->color} color.";
+        return "Hi, I'm {$this->name} and have an alcohol percentage of {$this->alcoholPercentage} and I have a {$this->color} color.}";
     }
 
     public function getAlcoholPercentage(): float
@@ -75,29 +75,39 @@ class Beer extends Beverage
     public function getColor(): string
     {
         return $this->color;
+
     }
 
     public function printBeerInfo()
     {
         echo $this->beerInfo();
+        echo "<br>";
     }
 }
+
+
 
 // Instantiate an object representing Duvel
 $duvel = new Beer('Duvel', 8.5, 'light', 3.5);
 
+// Print the getAlcoholPercentage in two different ways
+echo $duvel->getAlcoholPercentage() ;
+echo "<br>";
+echo "{$duvel->getAlcoholPercentage()}%" ;
+echo "<br>";
+
 // Print the color on the screen
 echo "Color: {$duvel->getColor()}";
-
+echo "<br>";
 // Print the getInfo on the screen
 echo $duvel->getInfo();
-
+echo "<br>";
 // Change the color of Duvel to light
 $duvel->__construct('Duvel', 8.5, 'light', 3.5);
-
+echo "<br>";
 // Print the new color on the screen
 echo "New Color: {$duvel->getColor()}";
-
+echo "<br>";
 // Print the beerInfo on the screen
 $duvel->printBeerInfo();
 
--- protected.php
+++ protected.php
@@ -11,3 +11,87 @@ TODO: Make all the other prints work without error without changing the beverage
 
 USE TYPEHINTING EVERYWHERE!
 */
+class Beverage
+{
+    protected $color;
+    protected $price;
+    protected $temperature;
+
+    public function __construct(string $color, float $price, string $temperature = 'cold')
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo(): string
+    {
+        return "This beverage is {$this->temperature} and {$this->color}.";
+    }
+
+    public function getTemperature(): string
+    {
+        return $this->temperature;
+    }
+}
+
+class Beer extends Beverage
+{
+    protected $name;
+    protected $alcoholPercentage;
+
+    public function __construct(string $name, float $alcoholPercentage, string $color = 'blond', float $price = 3.5, string $temperature = 'cold')
+    {
+        parent::__construct($color, $price, $temperature);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+
+    }
+
+    private function beerInfo(): string
+    {
+        return "Hi, I'm {$this->name} and have an alcohol percentage of {$this->alcoholPercentage} and I have a {$this->color} color.";
+    }
+
+    public function getAlcoholPercentage(): float
+    {
+        return $this->alcoholPercentage;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function printBeerInfo()
+    {
+        echo $this->beerInfo() . PHP_EOL;
+    }
+
+
+
+}
+// Instantiate an object representing Duvel
+$duvel = new Beer('Duvel', 8.5, 'light', 3.5);
+
+// Print the getAlcoholPercentage in two different ways
+echo $duvel->getAlcoholPercentage() ;
+echo "<br>";
+echo "{$duvel->getAlcoholPercentage()}%" ;
+echo "<br>";
+
+// Print the color on the screen
+echo "Color: {$duvel->getColor()}";
+echo "<br>";
+// Print the getInfo on the screen
+echo $duvel->getInfo();
+echo "<br>";
+// Change the color of Duvel to light
+$duvel->__construct('Duvel', 8.5, 'light', 3.5);
+echo "<br>";
+// Print the new color on the screen
+echo "New Color: {$duvel->getColor()}";
+echo "<br>";
+// Print the beerInfo on the screen
+$duvel->printBeerInfo();
+
